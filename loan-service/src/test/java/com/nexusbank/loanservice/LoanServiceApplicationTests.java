d762a8128da1661ee3fce2ef7d8472dcbc6f7358
kazaz03 <akazazovic1@etf.unsa.ba>
2026-03-29 21:28:35 +0200

    Initial project with 4 microservice projects

---
+++ loan-service/src/test/java/com/nexusbank/loanservice/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexusbank.loanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
