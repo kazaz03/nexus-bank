67270c482ec03d5233c6576b97628c58000fade1
kazaz03 <akazazovic1@etf.unsa.ba>
2026-05-21 23:52:40 +0200

    Added transaction and account SPA

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { DashboardComponent } from './pages/dashboard/dashboard';
 import { LoansComponent } from './pages/loans/loans';
+import { AccountsComponent } from './pages/accounts/accounts';
+import { TransferComponent } from './pages/transfer/transfer';
 import { authGuard } from './core/auth.guard';
-
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: 'loans', component: LoansComponent, canActivate: [authGuard] },
-  { path: '**', redirectTo: '/login' }
+  { path: '',           redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login',      component: LoginComponent },
+  { path: 'dashboard',  component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'accounts',   component: AccountsComponent,  canActivate: [authGuard] },
+  { path: 'transfer',   component: TransferComponent,  canActivate: [authGuard] },
+  { path: 'loans',      component: LoansComponent,     canActivate: [authGuard] },
+  { path: '**',         redirectTo: '/login' }
 ];
